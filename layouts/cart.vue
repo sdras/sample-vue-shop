f1bad87936e78e4cd61994b8762905afc301c8d6
sdras <sarah.drasner@gmail.com>
2017-11-24 19:24:34 -0700

    create cart

--- layouts/cart.vue
+++ layouts/cart.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <app-navigation />
-    <h3>carty cart</h3>
     <nuxt/>
   </div>
 </template>
@@ -17,82 +16,5 @@ export default {
 </script>
 
 <style>
-body,
-html {
-  padding: 0;
-  margin: 0;
-  background: #e6eefb;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-#app {
-  font-family: 'Barlow', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: white;
-}
-
-h1,
-h2,
-h3,
-h4 {
-  font-family: 'Playfair Display', serif;
-  font-weight: 700;
-}
-
-p,
-li {
-  font-family: 'Barlow', sans-serif;
-}
-
-/* Reset `button` and button-style `input` default styles */
-input[type='submit'],
-input[type='reset'],
-input[type='button'],
-button {
-  background: none;
-  border: 0;
-  color: inherit;
-  font: inherit;
-  line-height: normal;
-  overflow: visible;
-  padding: 0;
-  -webkit-appearance: button; /* for input */
-  -webkit-user-select: none; /* for button */
-  -moz-user-select: none;
-  -ms-user-select: none;
-}
-
-input::-moz-focus-inner,
-button::-moz-focus-inner {
-  border: 0;
-  padding: 0;
-}
-
-button {
-  padding: 10px 30px;
-  font-size: 13px;
-  font-weight: 600;
-  border-radius: 1000px;
-  cursor: pointer;
-  background: #ff2211;
-  color: white;
-  font-family: 'Barlow', sans-serif;
-  text-transform: uppercase;
-  margin: 10px;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
 </style>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,15 +1,72 @@
 <template>
-  <div>
+  <div class="cart capsule">
     <h1>Cart</h1>
+    <div class="cartitems" v-for="item in cart">
+      <div class="carttext">
+        <h4>{{ item.name }}</h4>
+        <p>{{ item.price | usdollar }} x {{ item.count }}</p>
+        <p>Total for this item: <strong>{{ item.price * item.count }}</strong></p>
+      </div>
+      <img class="cartimg" :src="`/${item.img}`" :alt="`Image of ${item.name}`">
+    </div>
+    <div class="total">
+      <h3>Total: </h3>
+    </div>
+    <button>Checkout Now</button>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'cart'
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    }
+  },
+  filters: {
+    usdollar: function(value) {
+      return `$${value}`;
+    }
+  }
 };
 </script>
 
 <style scoped>
+.cart {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  padding: 50px;
+  background: white;
+  border-radius: 3px;
+  margin-top: 10px;
+}
 
+.cartitems {
+  padding: 30px;
+  border-bottom: 1px solid #ccc;
+  width: 500px;
+}
+
+.carttext {
+  width: 250px;
+  float: left;
+}
+
+.carttext p,
+.carttext h4 {
+  padding: 5px;
+}
+
+.cartimg {
+  width: 100px;
+  border: 1px solid #ccc;
+  float: right;
+}
+
+.total {
+  margin: 20px 0;
+}
 </style>
