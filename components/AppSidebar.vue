8e9c04a9ec2bd44c5986e640f3f237814fd230da
sdras <sarah.drasner@gmail.com>
2017-11-25 22:24:56 -0700

    recreate range

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,15 +1,19 @@
 <template>
   <aside>
-    <app-multiselect />
+    <app-range />
+    <div class="sidearea">
+      <h4>Show Sale</h4>
+      
+    </div>
   </aside>
 </template>
 
 <script>
-import AppMultiselect from './AppMultiselect.vue';
+import AppRange from './AppRange.vue';
 
 export default {
   components: {
-    AppMultiselect
+    AppRange
   }
 };
 </script>
@@ -19,5 +23,17 @@ aside {
   background: white;
   height: 90vh;
   float: left;
+  padding: 20px;
+}
+
+.sidearea {
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 50px;
+}
+
+label {
+  font-family: 'Playfair Display', serif;
+  padding: 15px 0;
+  text-align: center;
 }
 </style>
