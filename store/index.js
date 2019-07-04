ebfa12603324f3066dcc874d3d407e26d45d9624
Sarah Drasner <sarah.drasner@gmail.com>
2019-07-04 14:21:54 -0600

    start building the app

--- store/index.js
+++ store/index.js
@@ -1,140 +1,139 @@
-import Vuex from 'vuex';
+import Vuex from "vuex"
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
       cartTotal: 0,
-      cart: {},
+      cart: [],
       sale: false,
       products: [
         {
-          name: 'Khaki Suede Polish Work Boots',
+          name: "Khaki Suede Polish Work Boots",
           price: 149.99,
-          category: 'women',
+          category: "women",
           sale: true,
-          article: 'shoe',
-          img: 'shoe1.png'
+          article: "shoe",
+          img: "shoe1.png"
         },
         {
-          name: 'Camo Fang Backpack Jungle',
+          name: "Camo Fang Backpack Jungle",
           price: 39.99,
-          category: 'women',
+          category: "women",
           sale: false,
-          article: 'jacket',
-          img: 'jacket1.png'
+          article: "jacket",
+          img: "jacket1.png"
         },
         {
-          name: 'Parka and Quilted Liner Jacket',
+          name: "Parka and Quilted Liner Jacket",
           price: 49.99,
-          category: 'men',
+          category: "men",
           sale: true,
-          article: 'jacket',
-          img: 'jacket2.png'
+          article: "jacket",
+          img: "jacket2.png"
         },
         {
-          name: 'Cotton Black Cap',
+          name: "Cotton Black Cap",
           price: 12.99,
-          category: 'men',
+          category: "men",
           sale: true,
-          article: 'hats',
-          img: 'hat1.png'
+          article: "hats",
+          img: "hat1.png"
         },
         {
-          name: 'Knit Sweater with Zips',
+          name: "Knit Sweater with Zips",
           price: 29.99,
-          category: 'women',
+          category: "women",
           sale: false,
-          article: 'sweater',
-          img: 'sweater1.png'
+          article: "sweater",
+          img: "sweater1.png"
         },
         {
-          name: 'Long Linen-blend Shirt',
+          name: "Long Linen-blend Shirt",
           price: 18.99,
-          category: 'men',
+          category: "men",
           sale: false,
-          article: 'shirt',
-          img: 'shirt1.png'
+          article: "shirt",
+          img: "shirt1.png"
         },
         {
-          name: 'Knit Orange Sweater',
+          name: "Knit Orange Sweater",
           price: 28.99,
-          category: 'men',
+          category: "men",
           sale: false,
-          article: 'sweater',
-          img: 'sweater2.png'
+          article: "sweater",
+          img: "sweater2.png"
         },
         {
-          name: 'Cotton Band-collar Blouse',
+          name: "Cotton Band-collar Blouse",
           price: 49.99,
-          category: 'men',
+          category: "men",
           sale: false,
-          article: 'shirt',
-          img: 'shirt2.png'
+          article: "shirt",
+          img: "shirt2.png"
         },
         {
-          name: 'Camo Fang Backpack Jungle',
+          name: "Camo Fang Backpack Jungle",
           price: 59.99,
-          category: 'women',
+          category: "women",
           sale: true,
-          article: 'jacket',
-          img: 'jacket3.png'
+          article: "jacket",
+          img: "jacket3.png"
         },
         {
-          name: 'Golden Pilot Jacket',
+          name: "Golden Pilot Jacket",
           price: 129.99,
-          category: 'women',
+          category: "women",
           sale: false,
-          article: 'jacket',
-          img: 'jacket4.png'
+          article: "jacket",
+          img: "jacket4.png"
         },
         {
-          name: 'Spotted Patterned Sweater',
+          name: "Spotted Patterned Sweater",
           price: 80.99,
-          category: 'women',
+          category: "women",
           sale: false,
-          article: 'jacket',
-          img: 'sweater4.png'
+          article: "jacket",
+          img: "sweater4.png"
         },
         {
-          name: 'Double Knit Sweater',
+          name: "Double Knit Sweater",
           price: 59.99,
-          category: 'men',
+          category: "men",
           sale: true,
-          article: 'jacket',
-          img: 'sweater5.png'
+          article: "jacket",
+          img: "sweater5.png"
         }
       ]
     },
     getters: {
-      women: state => filter(state.products, 'category', 'women'),
-      men: state => filter(state.products, 'category', 'men'),
-      sale: state => filter(state.products, 'sale', true)
+      women: state => filter(state.products, "category", "women"),
+      men: state => filter(state.products, "category", "men"),
+      sale: state => filter(state.products, "sale", true)
     },
     mutations: {
       switchSale: state => {
-        state.sale = !state.sale;
+        state.sale = !state.sale
       },
-      clearCartCount: state => {
-        state.cartTotal = 0;
+      addItem(state, payload) {
+        state.cartTotal += payload.qty
+        state.cart.push(payload)
       },
-      clearCartContents: state => {
-        state.cart = {};
+      removeCartItem(state, payload) {
+        state.cartTotal -= payload.qty
+        const cartItemPostion = state.cart.indexOf(payload)
+        state.cart.splice(cartItemPostion, 1)
       },
-      addItem: (state, item) => {
-        state.cartTotal++;
-        if (item.name in state.cart) {
-          state.cart[item.name].count++;
-        } else {
-          let stateItem = Object.assign({}, item);
-          stateItem.count = 1;
-          state.cart[item.name] = stateItem;
-        }
+      clearCartCount(state) {
+        state.cartTotal = 0
+      },
+      clearCartContents(state) {
+        state.cart = []
       }
     }
-  });
-};
+  })
+}
 
-export default createStore;
+export default createStore
 
 //helper
-const filter = (array, key, value) => array.filter(item => item[key] === value);
+const filter = (array, key, value) => array.filter(item => item[key] === value)
